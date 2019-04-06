7fa7d531d9c787aa9062df0550cd65533c5f05d1
wellington samuel <wellingtonsamuel2@gmail.com>
2019-04-06 12:46:24 -0400

    subindo dao-list

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # php7-DAO
+meu nome e wellington sou um estudante de  php da udemy
+to aprendendo php é uma linguagem muito interessante!
--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 spl_autoload_register(function($class_name){
- $filename = "class".DIRECTORY_SEPARATOR.$class_name.".php";//DIRECTORY_SEPARATOR SOBE UMA PASTA DO MESMO DIRETORIO
-if(file_exists(($filename))){
+ $filename = "class".DIRECTORY_SEPARATOR.$class_name.".php";//DIRECTORY_SEPARATOR SOBE UMA PASTA DO MESMO DIRETORIO SEMPRE BOM MANTER ORAGANIZADO
+if(file_exists(($filename))){// SE PRECISAR SUBIR MAIS DE UM DIRETORIO SERA MELHOR REPENSAR  NA ESTRUTURA DO DIRETORIO
 	require_once($filename);
 }
 
